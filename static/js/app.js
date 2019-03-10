78d553bf8b08927e84906ceb61cc086cc3e9d8aa
azaya <ann.marie.zaya@gmail.com>
2019-03-10 03:21:09 -0500

    edits to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -17,3 +17,34 @@ data.forEach(function(sightings) {
       cell.text(value);
     });
   });
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputValue = d3.select("#datetime").property('value');
+
+  //  clear text of the tds
+  d3.selectAll("td").property("textContent", "");
+
+  // delete all table rows in the table body
+  d3.select("tbody").selectAll("tr").remove();
+
+  var filteredData = tableData.filter(function(sightings) {
+    return sightings.datetime === inputValue;
+  })
+
+  // append the values of the filtered date to the table
+  filteredData.forEach((sightings) => {
+  var row = tbody.append("tr");
+  Object.entries(sightings).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+      })
+  });
+});
